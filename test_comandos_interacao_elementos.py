792c1db493cf09ef11ed007db4a52fc759b2d73d
Cristiane Mai <crismai.dev@gmail.com>
2024-06-24 10:36:23 +0200

    Comando Select - seleciona opção de um dropdown

---
+++ test_comandos_interacao_elementos.py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager 
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+
+
+browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+browser.implicitly_wait(12)
+
+browser.maximize_window()
+browser.get("https://chercher.tech/practice/practice-dropdowns-selenium-webdriver")
+
+dropdown_products = Select(browser.find_element(By.XPATH, "//select[@id='first']"))
+dropdown_products.select_by_visible_text("Google")
+
+dropdown_animals = Select(browser.find_element(By.XPATH, "//select[@id='animals']"))
+dropdown_animals.select_by_value("babycat")
+time.sleep(3)
+dropdown_animals.deselect_by_index("3")
+
+dropdown_food = Select(browser.find_element(By.XPATH, "//select[@id='second']"))
+dropdown_food.select_by_visible_text("Pizza")
+time.sleep(3)
+dropdown_food.select_by_visible_text("Burger")
