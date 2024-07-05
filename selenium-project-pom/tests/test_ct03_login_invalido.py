4ea042f718e07e62c7622472a919da1fcea98ca5
Cristiane Mai <crismai.dev@gmail.com>
2024-07-05 14:56:30 +0200

    Usando conftest, fixture e mark

---
+++ selenium-project-pom/tests/test_ct03_login_invalido.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+import conftest
+import pytest
+
+
+@pytest.mark.usefixtures("setup_teardown")
+@pytest.mark.login
+class TestCT03:
+    def test_ct03_login_invalido(self):
+        driver = conftest.driver
+        driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        driver.find_element(By.ID, "password").send_keys("zzzthhr")
+        driver.find_element(By.ID, "login-button").click()
+        assert len(driver.find_elements(By.XPATH, "//span[@class='title']")) == 0
