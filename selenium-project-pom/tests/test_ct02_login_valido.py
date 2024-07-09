c5810898a17923d4e8497c723f37b8ac6d5591e8
Cristiane Mai <crismai.dev@gmail.com>
2024-07-09 09:50:45 +0200

    Criando nossa LoginPage e o método fazer_login

--- selenium-project-pom/tests/test_ct02_login_valido.py
+++ selenium-project-pom/tests/test_ct02_login_valido.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 import conftest 
 import pytest
+from pages.login_page import LoginPage
 
 
 @pytest.mark.usefixtures("setup_teardown")
@@ -8,7 +9,8 @@ import pytest
 class TestCT02:
     def test_ct02_login_valido(self):
         driver = conftest.driver
-        driver.find_element(By.ID, "user-name").send_keys("standard_user")
-        driver.find_element(By.ID, "password").send_keys("secret_sauce")
-        driver.find_element(By.ID, "login-button").click()
+
+        login_page = LoginPage()
+        login_page.fazer_login("standart_user", "secret_sauce")
+      
         assert driver.find_element(By.XPATH, "//span[@class='title']").is_displayed()
